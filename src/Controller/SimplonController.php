3caadce8bdecef1e85fda502f5cc5bfb5e0ab9c7
albouchihoussem <albouchihoussemendasimplon@gmail.com>
2021-07-01 18:12:43 +0100

    modif

--- src/Controller/SimplonController.php
+++ src/Controller/SimplonController.php
@@ -15,5 +15,32 @@ class SimplonController extends AbstractController
     {
         return $this->render('simplon/index.html.twig');
     }
+    /**
+     * @Route("/getname/{nom}", name="getname")
+     */
+    public function getName($nom){
+   
+        return $this->render('simplon/detail.html.twig', ["Key"=>$nom]);
+    }
+    /**
+     * @Route("/list", name="list")
+     */
 
+    function Liste(){
+        $formations = [array('ref' => 'form147', 'Titre' => 'Formation Symfony
+        4','Description'=>'formation pratique',
+        'date_debut'=>'12/06/2020', 'date_fin'=>'19/06/2020',
+        'nb_participants'=>19) ,
+        array('ref'=>'form177','Titre'=>'Formation SOA' ,
+        'Description'=>'formation
+        theorique','date_debut'=>'03/12/2020','date_fin'=>'10/12/2020',
+        'nb_participants'=>0),
+        array('ref'=>'form178','Titre'=>'Formation Angular' ,
+   
+'Description'=>'formation
+        theorique','date_debut'=>'10/06/2020','date_fin'=>'14/06/2020',
+        'nb_participants'=>12)];
+        return $this->render("simplon/list.html.twig",['formation'=>$formations]);
+    }
+    
     }
